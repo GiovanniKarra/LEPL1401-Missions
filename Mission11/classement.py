c4c33ab25cc7d3441c261498d529072696f75773
Gogani <karragiovanni@gmail.com>
2021-12-09 17:55:57 +0100

    hi

--- Mission11/classement.py
+++ Mission11/classement.py
@@ -1,19 +1,15 @@
-class Classement:
-    """
-    Une implémentation primitive de classement, non ordonnée et de capacité fixe.
-    @author Kim Mens
-    @version 01 Décembre 2019
-    """
+from OrderedLinkedList import OrderedLinkedList
 
-    __maxcapacity = 10
+
+class Classement(OrderedLinkedList):
 
     def __init__(self):
         """
         @pre: -
         @post: un classement vide de taille 0 a été créé
         """
-        self.__resultats = {}  # dictionnaire de résultats actuelle (clé = coureur; valeur = résultat)
-        self.__size = 0  # nombre de résultats actuel (initialement 0, maximum __maxcapacity)
+        super().__init__([])
+        self.__resultats = {}
 
     def size(self):
         """
@@ -22,7 +18,7 @@ class Classement:
         @pre:  -
         @post: Le nombre de résultats actuellement stockés dans ce classement a été retourné.
         """
-        return self.__size
+        super().size()
 
     def add(self, r):
         """
@@ -30,15 +26,10 @@ class Classement:
         @pre:  r est une instance de la classe Resultat
         @post: Le résultat r a été inséré selon l'ordre du classement.
                En cas d'ex-aequo, r est inséré après les autres résultats de même ordre.
-        ATTENTION : L'implémentation actuelle ne respecte pas encore la post-condition!
-                    Le résultat est simplement ajouté à la dictionnaire, sans tenir compte de l'ordre.
-                    Une dictionnaire ne donne pas de garanties sur l'ordre des éléments.
         """
-        if self.size() >= self.__maxcapacity:
-            raise "Capacity of classement exceeded"
-
-        self.__size += 1
-        self.__resultats[r.coureur()] = r
+        super().add(r)
+        if self.__resultats[r.coureur().nom()].temps() > r.temps():
+            self.__resultats[r.coureur().nom()] = r.temps()
 
     def get(self, c):
         """
@@ -48,7 +39,10 @@ class Classement:
               classement. Retourne None si le coureur ne figure pas (encore)
               dans le classement.
         """
-        return self.__resultats.get(c)
+        try:
+            return self.__resultats[c]
+        except:
+            return None
 
     def get_position(self, c):
         """
@@ -58,13 +52,16 @@ class Classement:
               à partir de 1 pour la tête de ce classement. Si le coureur figure plusieurs fois
               dans le classement, la première (meilleure) position est retournée.
               Retourne -1 si le coureur ne figure pas dans le classement.
-        ATTENTION : L'implémentation actuelle ne respecte pas encore la post-condition!
-                    Etant donné que la dictionnaire de résultats ne connaît pas de position,
-                    pour le moment cette méthode retourne toujours "***position inconnue***".
-                    A vous de la corriger en utilisant une liste chaînée ordonnée
-                    comme structure de données, plutôt qu'une simple dictionnaire.
         """
-        return "***position inconnue***"
+        i = 1
+        result = self.first()
+        while c != result.value().coureur().nom() and i <= self.length():
+            result = result.next()
+            i += 1
+        if i > self.length():
+            return -1
+        else:
+            return i
 
     def remove(self, c):
         """
@@ -74,8 +71,23 @@ class Classement:
               c est comparé au sens de __eq__. Retourne c si un résultat a été retiré,
               of False si c n'est pas trouvé dans la liste.
         """
-        self.__size -= 1
-        return self.__resultats.pop(c, False)
+        if self.first().coureur() == c:
+            self.set_first(self.first().next())
+            self.dec_size()
+        else:
+            runner = self.first()
+        while runner.next() is not None and c != runner.next().coureur():
+            runner = runner.next()
+        if runner.next() is None:
+            return False
+
+        result = runner.next()
+        runner.set_next(result.next())
+        self.dec_size()
+        try:
+            return self.__resultats.pop(c.nom())
+        except:
+            return False
 
     def __str__(self):
         """
@@ -85,7 +97,8 @@ class Classement:
         @post: Retourne une représentation de ce classement sous forme d'un string,
                avec une ligne par résultat.
         """
-        d = self.__resultats
-        s = "".join("  " + str(self.get_position(c)) + " > " + str(d[c]) + "\n" for c in d)
-
+        s = "Classement : \n"
+        result = self.first()
+        while result is not None:
+            s += str(result)
         return s
